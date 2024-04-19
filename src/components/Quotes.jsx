b646513db5dfe4922fffe80f614459ca2e1e69cd
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-19 14:38:47 +0200

    :construction: Add site backbone

---
+++ src/components/Quotes.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function Quotes() {
+    const [quote, setQuote] = useState("");
+
+    function newQuote(){
+        fetch('https://api.gameofthronesquotes.xyz/v1/random')
+            .then(response => response.json())
+            .then(data => setQuote(data.sentence))
+            .catch(error => console.error(error));
+    }
+
+    useEffect(() => {
+        newQuote();
+    }, []);
+
+    return (
+        <div id="quote-container">
+            <div id="quote-box">
+                <div className="row">
+                    <p id="text">
+                        {quote}
+                    </p>
+                </div>
+                <div className="row">
+                    <p id="author">Alessio Manfredini</p>
+                </div>
+                <div className="row button-container">
+                    <button className="btn">
+                        <i class="bi bi-copy"></i>
+                    </button>
+                    <button className="btn">
+                        <i class="bi bi-arrow-counterclockwise"></i>
+                    </button>
+                </div>
+            </div>
+            <p>by DevManfre</p>
+        </div>
+    );
+}
+
+export default Quotes;
