6ff0c0d9e62a1c21df81c42660c59e204855fd2e
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-19 20:54:19 +0200

    :sparkles: Add fadeIn & fadeOut during quote change

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, trans } from "react"
 import '../static/css/Quotes.css'
 
 function Quotes() {
     const [quote, setQuote] = useState("");
     const [author, setAuthor] = useState("");
+    const [visible, setVisible] = useState(true);
 
     function newQuote() {
         fetch('https://api.gameofthronesquotes.xyz/v1/random')
@@ -19,10 +20,18 @@ function Quotes() {
         newQuote();
     }, []);
 
+    function handleClick() {
+        setVisible(false);
+        setTimeout(() => {
+            newQuote();
+            setVisible(true);
+        }, 750);
+    }
+
     return (
         <div id="quote-container">
             <div id="quote-box">
-                <div className="text">
+                <div className="text" style={{ opacity: visible ? 1 : 0, transition: 'opacity 0.5s ease-in-out' }}>
                     <div className="row">
                         <span id="text">
                             <i class="bi bi-quote"></i>
@@ -40,7 +49,7 @@ function Quotes() {
                     <a className="btn" id='tweet-quote' target="_blank" title="Tweet the quote!" href="https://twitter.com/intent/tweet">
                         <i class="bi bi-twitter-x"></i>
                     </a>
-                    <a className="btn" id='new-quote' onClick={newQuote} title="Get a new quote!">
+                    <a className="btn" id='new-quote' onClick={handleClick} title="Get a new quote!">
                         <i class="bi bi-arrow-counterclockwise"></i>
                     </a>
                 </div>
